e841ce4910030872731442b8d9cef9c916cb3155
Exxog <Exxog>
2025-10-01 18:58:12 +0200

    Add setup script for kind cluster with ingress-nginx and argocd-core

---
+++ setup-kind-cluster.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#create a kind cluster with ingress-nginx controller installed and configured.
+#https://kind.sigs.k8s.io/docs/user/ingress/
+cat <<EOF | sudo kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+    listenAddress: "127.0.0.1"
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+    listenAddress: "127.0.0.1"
+EOF
+sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+#deploy argocd-core in the cluster
+sudo kubectl apply -n argocd-core --create-namespace -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
