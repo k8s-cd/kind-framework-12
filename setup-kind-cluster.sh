92ecfc778c6dfb8b54dd828b4bf0f13b6e138f5b
Exxog <elapse-d>
2025-10-01 22:18:47 +0200

    Update setup script to deploy argocd-core in the argocd-infra namespace

--- setup-kind-cluster.sh
+++ setup-kind-cluster.sh
@@ -27,5 +27,37 @@ EOF
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 #deploy argocd-core in the cluster
-sudo kubectl --kubeconfig ~/.kube/kind-cd create namespace argocd-core
-sudo kubectl --kubeconfig ~/.kube/kind-cd apply -n argocd-core -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
+sudo kubectl --kubeconfig ~/.kube/kind-cd create namespace argocd-infra
+sudo kubectl --kubeconfig ~/.kube/kind-cd apply -n argocd-infra -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
+
+#wait for argocd-core to be ready
+echo "Waiting for argocd-core to be ready..."
+sudo kubectl --kubeconfig ~/.kube/kind-cd -n argocd-infra wait --for=condition=available --timeout=600s deployment/argocd-server
+
+sudo kubectl apply --kubeconfig ~/.kube/kind-cd -n argocd-infra  -f - <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: infra
+  namespace: argocd-infra
+spec:
+  ignoreDifferences:
+    - group: argoproj.io
+      kind: Application
+      jsonPointers:
+        - /operation
+  project: default
+  source:
+    repoURL: 'https://github.com/k8s-cd/kind-framework-12.git'
+    targetRevision: HEAD
+    path: infra
+
+  destination:
+    server: 'https://kubernetes.default.svc'
+    namespace: argocd-infra
+
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+EOF
