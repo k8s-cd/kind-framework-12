df2e482e8b953260ffc68f0e06b07c12325419a1
Exxog <elapse-d>
2025-10-03 10:47:57 +0200

    up

--- replace.sh
+++ replace.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 source .env
 
 find . -type f  -name  "*.sh" -o -name  "*.yaml" -o -name "*.yml" | while read -r file; do
-  for var in $(compgen -v | grep ^var_)
-    do   
-      val=${!var:-}
-      #echo "  Remplacement de $var par $val dans $file"
-      sed -i "s@'.*' #${var}@'${val}' #${var}@" "$file"
+  for var in $(compgen -v | grep ^var_); do   
+      val=${!var}
+      sed -i "s@'.*' #${var}@'${val}' #${var}@"     "$file"
     done
 done
