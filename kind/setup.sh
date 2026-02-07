f663ffafd537d7459011ee531720b01b11c768c4
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 19:11:17 +0100

    Update setup.sh

--- kind/setup.sh
+++ kind/setup.sh
@@ -1,7 +1,8 @@
 url=https://raw.githubusercontent.com/k8s-cd/common/main/
+export KIND_CLUSTER='automatic-cluster'
 
 #curl -sSL "${url}kind-sudo-cluster-with-nginx.sh" | bash -s -- arg1 arg2 arg3
 curl -sSL ${url}kind-sudo-cluster-with-nginx.sh | bash
 curl -sSL ${url}argocd-core-like.sh | bash
-curl -sSL ${url}argocd-core-app.sh | bash
+curl -sSL ${url}argocd-core-app.sh | bash -s -- 'kind/core' 'https://github.com/k8s-cd/kind-framework-12.git'
 
