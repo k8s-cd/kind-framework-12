68fae3644731ca8ea9cb7425bea2a69ea8073bc0
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-21 14:37:03 +0100

    Update setup.sh

--- kind/setup.sh
+++ kind/setup.sh
@@ -3,7 +3,7 @@ export KIND_CLUSTER='automatic-cluster'
 cmd="sudo -H --preserve-env=KIND_CLUSTER bash -s --"
 
 #curl -sSL "${url}kind-sudo-cluster-with-nginx.sh" | bash -s -- arg1 arg2 arg3
-curl -sSL ${url}kind-sudo-cluster-with-nginx.sh | cmd
-curl -sSL ${url}argocd-core-like.sh | cmd
-curl -sSL ${url}argocd-core-app.sh | cmd
+curl -sSL ${url}kind-sudo-cluster-with-nginx.sh | $cmd
+curl -sSL ${url}argocd-core-like.sh | $cmd
+curl -sSL ${url}argocd-core-app.sh | $cmd
 
